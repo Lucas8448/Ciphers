e718dfcdcd606a193b24154c2e59e2725bf23b64
Lucas Bateson <lucas.bateson@gmail.com>
2023-09-25 19:49:50 +0200

    Default pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Portal from './pages/Portal';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/">Hjem</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/portal">Portal</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/portal" element={<Portal />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Portal.js
@@ -0,0 +1,9 @@
+function Portal(){
+    return(
+        <div>
+            <h1>Portal</h1>
+        </div>
+    );
+}
+
+export default Portal;
