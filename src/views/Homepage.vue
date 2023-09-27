184e8ae05a66501b3aeae64a13d835c90835f3ff
Lucas Bateson <lucas.bateson@gmail.com>
2023-09-27 12:45:18 +0200

    Simple homepage

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,16 +1,21 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="container mx-auto min-h-screen flex flex-col justify-center items-center text-center">
-    <h1 class="text-5xl font-bold text-gray-900">Welcome to Ciphers!</h1>
-    <p class="text-lg mt-4 text-gray-700">Your go-to platform for all things awesome.</p>
-
-    <div class="mt-8 flex gap-4">
-      <router-link to="/login" class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded">
-        Login
-      </router-link>
-      <router-link to="/register" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
-        Register
-      </router-link>
+  <div class="text-gray-600 body-font">
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">The new way
+          <br class="hidden lg:inline-block">to talk
+        </h1>
+        <p class="mb-8 leading-relaxed">Tired of chat services taking your data without your permission, or not using secure protocols and encryptions. Look no further.</p>
+        <div class="flex justify-center">
+          <button href="/register" class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-400 rounded text-lg">Register</button>
+          <button href="/login" class="ml-4 inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-400 rounded text-lg">Login</button>
+        </div>
+      </div>
+      <button data-text="Awesome" class="button">
+        <span class="actual-text">&nbsp;Ciphers&nbsp;</span>
+        <span class="hover-text" aria-hidden="true">&nbsp;Ciphers&nbsp;</span>
+      </button>
     </div>
   </div>
 </template>
